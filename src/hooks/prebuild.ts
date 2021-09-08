2a4dbf13e7c98eeeb38a0d506bf25be622544a7d
shamsmosowi <shams.mosowi@gmail.com>
2021-09-08 16:10:54 +1000

    test

--- src/hooks/prebuild.ts
+++ src/hooks/prebuild.ts
@@ -16,8 +16,6 @@ async function start() {
     updateConfig('projectId',projectId)
     const settings = {
         rowyRunBuildStatus: "BUILDING",
-        rowyRunUrl: process.env.SERVICE_URL
-
     }
     await db.doc("_rowy_/settings").set(settings, { merge: true })
     await createRowyApp(projectId)
