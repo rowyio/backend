1744ed2bb3f9530f065223a75f189bffe36ed037
shamsmosowi <shams.mosowi@gmail.com>
2021-09-08 15:27:51 +1000

    .

--- src/hooks/postcreate.ts
+++ src/hooks/postcreate.ts
@@ -1,11 +1,17 @@
 import {db} from '../firebaseConfig'
 
 async function start() {
-    const update = {
-        rowyRunBuildStatus: "COMPLETE",
-        rowyRunUrl: process.env.SERVICE_URL
+    console.log('postcreate')
+    try {
+        const update = {
+            rowyRunBuildStatus: "COMPLETE",
+            rowyRunUrl: process.env.SERVICE_URL
+        }
+        await db.doc("/_rowy_/settings").update(update)  
+    } catch (error) {
+        console.log(error)
     }
-    await db.doc("/_rowy_/settings").update(update)
+
 }
 
 start();
