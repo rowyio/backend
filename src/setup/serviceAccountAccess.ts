d83d9f3136419b0e18c3a6555eb203e93b543696
shamsmosowi <shams.mosowi@gmail.com>
2021-09-13 20:44:32 +1000

    change method

--- src/setup/serviceAccountAccess.ts
+++ src/setup/serviceAccountAccess.ts
@@ -6,9 +6,12 @@ export const serviceAccountAccess =  async (req:Request, res:Response) => {
     const url = req.body.url;
     try {
         // VM instance metadata service account
-        //`http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/email`
-        const response = await fetch(url??`http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/email`);
-        const body = await response.json();
+        //`http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/email` 
+        const response = await fetch(url??`http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/email`,{
+        method: 'get',
+        headers:  {'Metadata-Flavor': 'Google'}
+        });
+        const body = response.json();
         return res.send({body})
     } catch (error) {
         return res.send({error})
