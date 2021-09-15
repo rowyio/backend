db556676ca53d7da6c6a15f837f47990611bbc09
shamsmosowi <shams.mosowi@gmail.com>
2021-09-15 22:50:47 +1000

    ascii art

---
+++ src/asciiLogo.ts
@@ -0,0 +1,19 @@
+export const logo = `                                                                                                                                                                                                                                                                                                    
+rrrrrrrrrrrrrrrrr     ooooooooooo wwwwwww           wwwww           wwwwwwwyyyyyyy           yyyyyyy
+r:::rrr:::::::::r  oo:::::::::::oow:::::w         w:::::w         w:::::w  y:::::y         y:::::y 
+r:::::::::::::::r o:::::::::::::::ow:::::w       w:::::::w       w:::::w    y:::::y       y:::::y  
+r::::::rrrrrrrrrr o:::::ooooo:::::o w:::::w     w:::::::::w     w:::::w      y:::::y     y:::::y   
+r:::::r           o::::o     o::::o  w:::::w   w:::::w:::::w   w:::::w        y:::::y   y:::::y    
+r:::::r           o::::o     o::::o   w:::::w w:::::w w:::::w w:::::w          y:::::y y:::::y     
+r:::::r           o::::o     o::::o    w:::::w:::::w   w:::::w:::::w            y:::::y:::::y      
+r:::::r           o::::o     o::::o     w:::::::::w     w:::::::::w              y:::::::::y       
+r:::::r           o:::::ooooo:::::o      w:::::::w       w:::::::w                y:::::::y        
+r:::::r           o:::::::::::::::o       w:::::w         w:::::w                  y:::::y         
+r:::::r            oo:::::::::::oo         w:::w           w:::w                  y:::::y          
+rrrrrrr              ooooooooooo            www             www                  y:::::y           
+                                                                               y:::::y            
+                                                                              y:::::y             
+                                                                             y:::::y              
+                                                                            y:::::y               
+                                                                           yyyyyyy                
+                                                                                                    `;
--- src/hooks/postcreate.ts
+++ src/hooks/postcreate.ts
@@ -1,23 +1,23 @@
 import { db } from "../firebaseConfig";
 import chalk from "chalk";
+import { logo } from "../asciiLogo";
 async function start() {
-  console.log("postcreate");
-  try {
-    const update = {
-      rowyRunBuildStatus: "COMPLETE",
-      rowyRunUrl: process.env.SERVICE_URL,
-    };
-    await db.doc("/_rowy_/settings").update(update);
-    console.log(chalk.green("Successfully created rowy app"));
-    const rowyAppURL = `https://${process.env.GOOGLE_CLOUD_PROJECT}.rowy.app/setup?rowyRunUrl=${process.env.SERVICE_URL}`;
-    console.log(
-      chalk
-        .hex("#4200ff")
-        .bold(`Open ${rowyAppURL} to continue the setup process`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+  const rowyRunUrl = process.env.SERVICE_URL;
+  const rowyAppURL = `https://${process.env.GOOGLE_CLOUD_PROJECT}.rowy.app/setup?rowyRunUrl=${process.env.SERVICE_URL}`;
+  const update = {
+    rowyRunBuildStatus: "COMPLETE",
+    rowyRunUrl,
+  };
+  await db.doc("/_rowy_/settings").update(update);
+  console.log(chalk.green("Successfully created rowy app"));
+  console.log(logo);
+  console.log(
+    `
+    ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    +  Continue the setup process by going to the link bellow     +
+    +  ${rowyAppURL}
+    ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++`
+  );
 }
 
 start();
