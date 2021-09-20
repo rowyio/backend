21ffd3afbd2b18c3714cdb030e21807683ca8d66
shamsmosowi <shams.mosowi@gmail.com>
2021-09-20 14:59:12 +1000

    cors methods

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: "*",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
   })
 );
 const functionWrapper = (fn) => async (req, res) => {
