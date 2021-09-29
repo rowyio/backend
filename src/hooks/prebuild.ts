dd9a5787c365ff74cd12e2d7c6c6e65600e6fd81
shamsmosowi <shams.mosowi@gmail.com>
2021-09-29 14:03:10 +1000

    check if firestore exist

--- src/hooks/prebuild.ts
+++ src/hooks/prebuild.ts
@@ -1,12 +1,34 @@
 import { updateConfig, getProjectId } from "./utils";
 import { db } from "../firebaseConfig";
 import { logError } from "./createRowyApp";
+import { asyncExecute } from "../functionBuilder/compiler/terminal";
 
 async function start() {
   const projectId = getProjectId();
+
   if (!projectId) {
     throw new Error("GOOGLE_CLOUD_PROJECT env variable is not set");
   }
+  asyncExecute(
+    "gcloud services list --project rowy-service",
+    (error, stdout, stderr) => {
+      /*
+    example output:
+    appengine.googleapis.com                App Engine Admin API
+bigquery.googleapis.com                 BigQuery API
+bigquerydatatransfer.googleapis.com     BigQuery Data Transfer API
+    */
+      // get the services list urls
+
+      const services = stdout.split("\n");
+      const servicesUrls = services.map((service) => {
+        return service.split(" ")[0];
+      });
+      if (!servicesUrls.includes("firestore.googleapis.com")) {
+        console.log("Missing Firestore");
+      }
+    }
+  );
   try {
     updateConfig("projectId", projectId);
     updateConfig("region", process.env.GOOGLE_CLOUD_REGION);
