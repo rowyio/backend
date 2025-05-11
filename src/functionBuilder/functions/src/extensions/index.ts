37ecf022ecfbb453fb1b4ae9e33bf2dd2552f9e7
shams-mosowi <shams@buildship.com>
2025-05-10 22:29:27 -0700

    upgrade cloud function

--- src/functionBuilder/functions/src/extensions/index.ts
+++ src/functionBuilder/functions/src/extensions/index.ts
@@ -1,4 +1,4 @@
-import * as functions from "firebase-functions";
+import * as functions from "firebase-functions/v1";
 import utilFns, { hasRequiredFields, getTriggerType } from "../utils";
 import { db, auth, storage } from "../firebaseConfig";
 import { LoggingFactory } from "../logging";
--- src/functionBuilder/functions/src/index.ts
+++ src/functionBuilder/functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from "firebase-functions";
+import * as functions from "firebase-functions/v1";
 import derivative from "./derivatives";
 import extension from "./extensions";
 import * as config from "./functionConfig";
